5b72ab72b4868bf97148e17b55aa6d937ec1bbc1
Shuai Gao <shuai.gao@rea-group.com>
2019-04-14 16:47:37 +0800

    Add programingModel.js

---
+++ programingModel.js
@@ -0,0 +1,6 @@
+exports.showEventAndContext = (event, context) => {
+  console.log('Show event: =====================================')
+  console.log(JSON.stringify(event))
+  console.log('Show context: =====================================')
+  console.log(JSON.stringify(context))
+}
